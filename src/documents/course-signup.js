a3a3e20e002a1c0252edb1138dacb0fc776bbc51
Yeehaa <medialoperations@gmail.com>
2014-04-21 00:20:22 +0200

    improve authentication

--- src/documents/course-signup.js
+++ src/documents/course-signup.js
@@ -1,10 +1,91 @@
-//var angular = require('angular');
-
-// create angular app
 var validationApp = angular.module('validationApp', ['firebase']);
 
-// create angular controller
-validationApp.controller('mainController', function($scope, $firebaseSimpleLogin, $firebase) {
+validationApp.constant('firebaseUrl', 'https://cth-site.firebaseio.com/users/');
+
+validationApp.service('firebase', function(firebaseUrl){
+  return new Firebase(firebaseUrl);
+});
+
+validationApp.service('loginService', function(firebase, $firebaseSimpleLogin){
+  return $firebaseSimpleLogin(firebase);
+})
+
+validationApp.service('users', function(firebase, $firebase){
+  return $firebase(firebase);
+})
+
+
+validationApp.service('authentication', function($q, userService, loginService){
+
+  var login = function(){
+    var deferred = $q.defer();
+
+    loginService.$login('github')
+      .then(function(data){
+        var user = {
+          githubId: data.id,
+          avatar_url: data.thirdPartyUserData.avatar_url,
+          userName: data.username,
+          fullName: data.displayName,
+          email: data.thirdPartyUserData.emails[0].email
+        } 
+
+        var userFound = userService.findUserById(data.id);
+
+        if (userFound) {
+          deferred.resolve(userFound);
+        } else {
+          deferred.reject(user); 
+        }
+    });
+
+    return deferred.promise;
+  };
+
+  var logout = function(){
+    loginService.$logout();
+  }
+
+  return {
+    login: login,
+    logout: logout,
+  }
+});
+
+validationApp.service('userService', function(users){
+
+  var findUserById = function(githubId) {
+
+    var userFound = false;
+    var gitHubKeys = users.$getIndex();
+
+    gitHubKeys.forEach(function(key) {
+      var user = users[key];
+      if (user.githubId === githubId) {
+        userFound = user;
+      }
+    });
+    return userFound;
+  };
+
+  return {
+    users: users,
+    findUserById: findUserById
+  }
+});
+
+
+validationApp.controller('registrationController', function($scope, userService){
+
+  var users = userService.users;
+
+  $scope.$on('user:loggedin', userEvents);
+  $scope.$on('user:loggedout', userEvents);
+    
+  function userEvents (evt, user, current){
+    console.log(evt);
+    console.log(user);
+  };
 
 	$scope.yearOptions = {
 		"ba1": "Bachelor year 1",
@@ -15,79 +96,59 @@ validationApp.controller('mainController', function($scope, $firebaseSimpleLogin
 		"oth": "Other"
 	}
 
-  var authFire = new Firebase('https://cth-site.firebaseio.com/');
+	$scope.submitForm = function(isValid) {
 
-  $scope.loginService = $firebaseSimpleLogin(authFire);
+		var user = {
+      'githubId': $scope.newUser.githubId,
+      'avatarUrl': $scope.newUser.avatar_url,
+			'userName': $scope.newUser.userName,
+      'name': {
+        'first': $scope.newUser.name.first,
+        'last': $scope.newUser.name.last
+      },
+			'email': $scope.newUser.email,
+			'studno': $scope.newUser.studno,
+			'studyYear': $scope.newUser.studyYear,
+			'study': $scope.newUser.study,
+			'codingExperience': $scope.newUser.codingExperience,
+			'motivation': $scope.newUser.motivation
+		}
+
+		if (isValid) {
+			users.$add(user).then(function(ref){
+        $scope.$emit('user:registered', user);
+      }, function(err){
+        alert("Don't try this at home");
+      });
+		}
+	};
+});
+
+
+validationApp.controller('mainController', function($scope, authentication) {
+
   $scope.currentUser = false;
-  $scope.users = $firebase(authFire);
-  $scope.showForm = false;
+  $scope.newUser = false;
 
-  $scope.users.$on("loaded", function() {
-    $scope.gitHubKeys = $scope.users.$getIndex();
+  $scope.$on('user:registered', function(evt, user, current){
+    $scope.currentUser = user;
+    $scope.newUser = false;
   });
 
-  $scope.users.$on("value", function() {
-    $scope.gitHubKeys = $scope.users.$getIndex();
-  });
-
-	$scope.usersFire = $firebase(authFire);
   $scope.login = function(){
-    $scope.loginService.$login('github')
-      .then(function(data){
-        user = {
-          githubId: data.id,
-          avatar_url: data.thirdPartyUserData.avatar_url,
-          userName: data.username,
-          fullName: data.displayName,
-          email: data.thirdPartyUserData.emails[0].email
-        } 
-        $scope.currentUser = user;
-        $scope.currentUser.registered = false;
-        $scope.showForm = true;
-        $scope.checkRegisteredUser(data.id);
+    authentication.login()
+      .then(function(registeredUser){
+        $scope.currentUser = registeredUser;
+        $scope.$broadcast('user:loggedin', registeredUser);
+      },
+      function(newUser){
+        $scope.newUser = newUser;
       });
   };
 
   $scope.logout = function(){
+    $scope.$broadcast('user:loggedout', $scope.currentUser);
     $scope.currentUser = false;
-    $scope.showForm = false;
-    $scope.loginService.$logout();
-    console.log($scope.currentUser);
-    console.log($firebase(authFire));
+    authentication.logout();
   }
-
-  $scope.checkRegisteredUser = function(githubId) {
-    $scope.gitHubKeys.forEach(function(key) {
-      var user = $scope.users[key];
-      if(user.githubId === githubId) {
-        $scope.currentUser = user;
-        $scope.showForm = false;
-        $scope.currentUser.registered = true;
-        console.log($scope.currentUser);
-      }
-    });
-  };
-
-	$scope.submitForm = function(isValid) {
-
-		var user = {
-      'githubId': $scope.currentUser.githubId,
-      'avatarUrl': $scope.currentUser.avatar_url,
-			'userName': $scope.currentUser.userName,
-      'fullName': $scope.currentUser.fullName,
-			'email': $scope.currentUser.email,
-			'studno': $scope.currentUser.studno,
-			'studyYear': $scope.currentUser.studyYear,
-			'study': $scope.currentUser.study,
-			'codingExperience': $scope.currentUser.codingExperience,
-			'motivation': $scope.currentUser.motivation
-		}
-
-		if (isValid) {
-      $scope.showForm = false;
-      $scope.currentUser.registered = true;
-			$scope.users.$add(user);
-		}
-	};
-
 });
